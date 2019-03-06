0c8b62d982620508eb821ef4ca659280b211955b
SteveMustanski <steve.mustanski@gmail.com>
2019-03-06 17:07:16 -0500

    added Header component with routing to home and about

---
+++ src/components/common/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, IndexLink } from "react-router";
+
+const Header = props => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">
+        Home
+      </IndexLink>
+      {"|"}
+      <Link to="/about" activeClassName="active">
+        About
+      </Link>
+    </nav>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
