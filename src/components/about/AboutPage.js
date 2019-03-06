768f2380fed18af4c109bd69051887987e880d5b
SteveMustanski <steve.mustanski@gmail.com>
2019-03-06 13:06:39 -0500

    added home and about draft components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from "react";
+
+class AboutPage extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          This application users React, Redux, React Router and several other
+          libraries.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
