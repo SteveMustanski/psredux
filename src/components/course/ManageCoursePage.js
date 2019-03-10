7a7217e16a9d84879746a30c84178af3dbfd2f46
SteveMustanski <steve.mustanski@gmail.com>
2019-03-10 16:23:53 -0400

    added save and update courses

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -1,39 +1,97 @@
-import React, { Component } from "react";
+import React, { PropTypes } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
 import CourseForm from "./CourseForm";
-import PropTypes from "prop-types";
+//import { authorsFormattedForDropdown } from "../../selectors/selectors";
+import toastr from "toastr";
 
-class ManageCoursePage extends Component {
+export class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
       course: Object.assign({}, this.props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
+
+    this.saveCourse = this.saveCourse.bind(this);
+    this.updateCourseState = this.updateCourseState.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id != nextProps.course.id) {
+      // Necessary to populate form when existing course is loaded directly.
+      this.setState({ course: Object.assign({}, nextProps.course) });
+    }
+  }
+
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = Object.assign({}, this.state.course);
+    course[field] = event.target.value;
+    return this.setState({ course: course });
+  }
+
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = "Title must be at least 5 characters.";
+      formIsValid = false;
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  saveCourse(event) {
+    event.preventDefault();
+    this.props.actions.saveCourse(this.state.course);
+  }
+
+  redirect() {
+    this.setState({ saving: false });
+    toastr.success("Course saved.");
+    this.context.router.push("/courses");
   }
 
   render() {
     return (
-      <div>
-        <CourseForm
-          allAuthors={this.props.authors}
-          course={this.state.course}
-          errors={this.state.errors}
-        />
-      </div>
+      <CourseForm
+        course={this.state.course}
+        onChange={this.updateCourseState}
+        onSave={this.saveCourse}
+        errors={this.state.errors}
+        allAuthors={this.props.authors}
+        saving={this.state.saving}
+      />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
+//Pull in the React Router context so router is available on this.context.router.
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object
+};
+
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course) return course[0]; //since filter returns an array, have to grab the first.
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; // from the path `/course/:id`
+
   let course = {
     id: "",
     watchHref: "",
@@ -42,17 +100,15 @@ function mapStateToProps(state, ownProps) {
     length: "",
     category: ""
   };
-  // format the author data in state for the drop down
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + " " + author.lastName
-    };
-  });
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: []
+    //authorsFormattedForDropdown(state.authors)
   };
 }
 
@@ -62,4 +118,7 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default ManageCoursePage;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
