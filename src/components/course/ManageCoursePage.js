9800db42bee68cfaa2331ef0157bd448ff5293cc
SteveMustanski <steve.mustanski@gmail.com>
2019-03-09 23:08:39 -0500

    added CourseForm and SelectInput, TextInput components to it

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,26 +2,47 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
+import CourseForm from "./CourseForm";
+import PropTypes from "prop-types";
 
 class ManageCoursePage extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
       <div>
-        <h1>Manage Course</h1>
+        <CourseForm
+          course={this.state.course}
+          errors={this.state.errors}
+          allAuthors={[]}
+        />
       </div>
     );
   }
 }
 
-ManageCoursePage.propTypes = {};
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
+  let course = {
+    id: "",
+    watchHref: "",
+    title: "",
+    authorId: "",
+    length: "",
+    category: ""
+  };
   return {
-    state: state
+    state: course
   };
 }
 
